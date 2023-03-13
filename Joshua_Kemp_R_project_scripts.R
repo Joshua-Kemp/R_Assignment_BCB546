57c24263b0bd3950a53cc75d10f686efe4bae9b4
Joshua-Kemp <jmkemp@iastate.edu>
2023-03-12 23:54:41 -0500

    Everything works

---
+++ Joshua_Kemp_R_project_scripts.R
@@ -0,0 +1,125 @@
+#install.packages("tidyverse", "dplyr", "sjmisc")
+#install.packages("sjmisc")
+
+library(tidyverse)
+library(dplyr)
+library(rmarkdown)
+library(sjmisc)
+library(stats)
+
+
+## Download files and transpose fang
+snppositions <- read_tsv("https://github.com/EEOB-BioData/BCB546_Spring2023/raw/main/assignments/UNIX_Assignment/snp_position.txt", col_names=TRUE, show_col_types = FALSE) %>% select(SNP_ID, Chromosome, Position)
+fang.original<- read_tsv("https://github.com/EEOB-BioData/BCB546_Spring2023/blob/main/assignments/UNIX_Assignment/fang_et_al_genotypes.txt?raw=true", col_names=FALSE, show_col_types = FALSE) 
+#Col.names = FALSE makes sure that the header will be a row so that we have the SNP column when we transpose.  I had added the column using mutate, but this is cleaner.
+
+
+### There are genotypes in the fang file that do not belong to teosinte or maize groups we filter for
+976 + 1574
+unique(duplicated(fang.original$X1))
+unique(fang.original$X3)
+unique(duplicated(snppositions$SNP_ID))
+unique(snppositions$Chromosome)
+
+
+#seperate by species and traspose
+maize.fang <- filter(fang.original, X3 == "ZMMIL" | X3 == "ZMMLR" | X3 =="ZMMMR" | X1 == "Sample_ID") %>% rotate_df() 
+teosinte.fang <- filter(fang.original, X3 == "ZMPBA" | X3 == "ZMPIL" | X3 =="ZMPJA" | X1 == "Sample_ID") %>% rotate_df() 
+#filter(teosinte.fang, V1 == "Sample_ID") -> colnames(teosinte.fang)
+#filter(maize.fang, V1 == "Sample_ID") -> colnames(maize.fang)
+
+##workflow 1. add sample names to col names because it is nice 2. merge genofiles with snpostions 3. remove unknown missing multiple and "" positions and write to files 4. loop or lapply through chr #s filtering and sorting
+#create functions and lists
+#lists
+species.files.names <- c("maize.fang", "teosinte.fang")
+chromosomes <- c(1:10)
+problem.locations <- c("unknown", "multiple", NA, "missing")
+
+## functions
+Firstrow.to.colnames <- function(dfx) {
+  newcolnames <- filter(dfx, V1 == "Sample_ID")
+  colnames(dfx) <- c(newcolnames)
+  dfx }
+Merge.with.snppositions <- function(f) merge.data.frame(snppositions, f, by.x = "SNP_ID", by.y = "Sample_ID", all.y = TRUE, sort = FALSE)
+Remove.problem.postions <- function(dfx) postions.problems <- filter(dfx, Position %in% problem.locations)
+Replace_quest_with_hyphen <- function(dfx) as.data.frame(lapply(dfx, function(x) (gsub(pattern="?", replacement="-", x, fixed = TRUE))) -> dfx)
+Filter.by.chromosome <- function(dfx) {
+  append(
+  lapply(chromosomes, function(y) filter(dfx, Chromosome == y )) %>% lapply(function(x) filter(x, !(Position %in% problem.locations)) %>% arrange(as.numeric(Position))),
+  lapply(chromosomes, function(y) filter(Replace_quest_with_hyphen(dfx), Chromosome == y )) %>% lapply(function(x) filter(x, !(Position %in% problem.locations)) %>% arrange(desc(as.numeric(Position))))
+  )
+  }
+Create.Split.Filenames <- {
+  sapply(1:2, function(x) {
+  c(
+    sapply(chromosomes, function(y) c(paste0(species.files.names[x], "_chr-", y, "_ascending"))),
+    sapply(chromosomes, function(y) c(paste0(species.files.names[x], "_chr-", y, "_descending"))))
+  })
+  }
+Write_csv <- function(df) walk2(df, paste0(names(df), ".csv"), write_csv)
+
+## Workflow
+#### Add Marker locations (Chromosome and Postion) to our genotype files then name them
+genotypes.by.species <- list(maize.fang, teosinte.fang)
+genotypes.by.species <- lapply(genotypes.by.species, Firstrow.to.colnames)
+merged.genotypes.by.species <-lapply(genotypes.by.species, Merge.with.snppositions)
+names(merged.genotypes.by.species) <- c(paste("merged_", species.files.names, sep = ""))
+problem.positions <- lapply(merged.genotypes.by.species, Remove.problem.postions)
+names(problem.positions) <- paste0(species.files.names, "_miss_unk_mult", sep = "")
+problem.positions_hyphen <- (lapply(problem.positions, function(l) Replace_quest_with_hyphen(l)))
+names(problem.positions_hyphen) <- paste0(species.files.names, "_hypen_miss_unk_mult", sep = "")
+#### Split dataframes by Chromosome
+split.genotype.files <- lapply(merged.genotypes.by.species, Filter.by.chromosome)
+Create.Split.Filenames -> newsplitnames
+#### Flatten List Structure (remove nesting) and Name the split files
+flat.split.genos <- list_flatten(split.genotype.files)
+names(flat.split.genos) <- newsplitnames
+#### write lists to csv files
+Write_csv(flat.split.genos)
+Write_csv(problem.positions)
+Write_csv(problem.positions_hyphen)
+
+
+## Visualization
+
+### Reformatting
+
+both_species.dataframe <- full_join(merged.genotypes.by.species[[1]], merged.genotypes.by.species[[2]], by = join_by("SNP_ID" == "SNP_ID", "Chromosome" == "Chromosome", "Position" == "Position"), suffix = c("", ""))
+transposed_full.dataframe <- rotate_df(both_species.dataframe)
+transposed_full.dataframe <- rotate_df(both_species.dataframe) %>% mutate(Species = ifelse(V986 == "ZMMIL" | V986 == "ZMMLR" | V986 =="ZMMMR", "Maize", ifelse(V986 == "ZMPBA" | V986 == "ZMPIL" | V986 =="ZMPJA", "Teosinte", NA)), .before = V1)
+transposed_full.dataframe[1,1] <- "Species"
+colnames(transposed_full.dataframe) <- filter(transposed_full.dataframe, Species == "Species")
+reformatted_one_genotypecall_per_row.df <- filter(transposed_full.dataframe, !(Sample_ID == "NA" | Sample_ID == "Sample_ID")) %>% pivot_longer(cols = -c("Species", "Group", "JG_OTU", "Sample_ID")) %>% 
+  merge.data.frame(snppositions, by.x = "name", by.y = "SNP_ID", all.x = TRUE, sort = FALSE) %>% 
+  relocate(c(Chromosome, Position, Species, Group), .before = name) %>% 
+  mutate(Allele1 = substr(value, 1, 1), .before = value) %>% 
+  mutate(Allele2 = substr(value, 3, 3), .before = value) %>% 
+  mutate(Homozygous = Allele1 == Allele2 & Allele1 != "?", .before = Allele1) %>% 
+  mutate(Missing_call = Allele1 == "?" | Allele2 == "?", .before = Allele1) %>% 
+  filter(!(Position %in% problem.locations)) %>% 
+  mutate(bin_position = cut(as.integer(Position), 20, labels = FALSE), .after = Position)
+
+### Plotting
+#### SNPs per Chromosome
+ggplot(data = reformatted_one_genotypecall_per_row.df ) + geom_bar(mapping = aes(x= as.factor(as.integer(Chromosome)), color = Species )) +
+  ylab('Number of tested SNPs') +
+  xlab('Chromosome') 
+
+#### Distribution accross Chromosomes
+ggplot(data = reformatted_one_genotypecall_per_row.df) +
+  aes(as.double(Position)/98507715) +
+  geom_histogram(aes(y = ..density..), alpha = 0.4, bins = 20) + 
+  geom_density(aes(as.numeric(Position)/98507715),) + 
+  facet_wrap(~ as.double(Chromosome)) +
+  ggtitle("SNPs accross Chromosomes") +
+  xlab('Genome position (Mb)') +
+  ylab('SNP density')
+
+
+## this ends up being more than can easily plotted and may crash the computer.  That said, there are a number of lines that were very far from inbred, and may not have correct phenotype to genotype matching. Overall, lines are less inbred than you would expect for this kind of study.
+contaminated_lines <- summarise(reformatted_one_genotypecall_per_row.df, Mean_Homozygosity = mean(Homozygous), Species = mode(Species), .by = Sample_ID)
+contaminated_lines <- arrange(contaiminated_lines, Mean_Homozygosity)
+head(contaminated_lines, n = 20)
+ggplot(data = contaiminated_lines) + geom_point(mapping = aes(x= Sample_ID, y= Mean_Homozygosity, color = Species))
+
+
--- Work_in_progress.R
+++ Work_in_progress.R
@@ -11,20 +11,22 @@ library(tidyverse)
 library(dplyr)
 library(rmarkdown)
 library(sjmisc)
-
+library(magrittr)
 ## Download files and transpose fang
 snppositions <- read_tsv("https://github.com/EEOB-BioData/BCB546_Spring2023/raw/main/assignments/UNIX_Assignment/snp_position.txt", col_names=TRUE, show_col_types = FALSE) %>% select(SNP_ID, Chromosome, Position)
 fang.original<- read_tsv("https://github.com/EEOB-BioData/BCB546_Spring2023/blob/main/assignments/UNIX_Assignment/fang_et_al_genotypes.txt?raw=true", col_names=FALSE, show_col_types = FALSE) 
+select(fang.original, X1) -> row.names(fang.original)
 #Col.names = FALSE makes sure that the header will be a row so that we have the SNP column when we transpose.  I had added the column using mutate, but this is cleaner.
 
 #seperate by species and traspose
-maize.fang <- filter(fang.original, X3 == "ZMMIL" | X3 == "ZMMLR" | X3 =="ZMMMR" | X1 == "Sample_ID") %>% rotate_df()
-teosinte.fang <- filter(fang.original, X3 == "ZMPBA" | X3 == "ZMPIL" | X3 =="ZMPJA" | X1 == "Sample_ID") %>% rotate_df()
-
+maize.fang <- filter(fang.original, X3 == "ZMMIL" | X3 == "ZMMLR" | X3 =="ZMMMR" | X1 == "Sample_ID") %>% rotate_df() 
+teosinte.fang <- filter(fang.original, X3 == "ZMPBA" | X3 == "ZMPIL" | X3 =="ZMPJA" | X1 == "Sample_ID") %>% rotate_df() 
+filter(teosinte.fang, V1 == "Sample_ID") -> colnames(teosinte.fang)
+filter(maize.fang, V1 == "Sample_ID") -> colnames(maize.fang)
 
 #Test renaming columns to the sample_ID
 fang_labeled <- originalfang
-colnames(fang_labeled) <- filter(fang_labeled, V1 == "Sample_ID")
+colnames(fang_labeled) <- filter(V1 == "Sample_ID")
 
 
 merge.df <- merge.data.frame(snppositions, fang_labeled, by.x = "SNP_ID", by.y = "Sample_ID", all.y = TRUE, sort = FALSE)
@@ -36,26 +38,59 @@ labeled.merge.df <- merge.data.frame(snppositions, fang.transposed, by.x = "SNP_
   #lists
   species.files.names <- c("maize.fang", "teosinte.fang")
   genotypes.by.species <- list(maize.fang, teosinte.fang)
-  chromosomes <- c(1:10)
+  chromosomes <<- c(1:10)
   problem.locations <- c("unknown", "multiple", NA, "missing")
-  unk_miss_mult.locations <- c
-  #fuctions
-  Merge.with.snppositions <- function(f) merge.data.frame(snppositions, f, by.x = "SNP_ID", by.y = "V1", all.y = TRUE, sort = FALSE)
+
   
+  -> paste(names(dfx), "_CHR", chr, sep = "")
+  #functions
+  Merge.with.snppositions <- function(f) merge.data.frame(snppositions, f, by.x = "SNP_ID", by.y = "Sample_ID", all.y = TRUE, sort = FALSE)
   Firstrow.to.colnames <- function(dfx) colnames(dfx)<-c(filter(dfx, SNP_ID == "Sample_ID"))
   Remove.problem.postions <- function(dfx) postions.problems <- filter(dfx, Chromosome %in% problem.locations)
+  
+  
+  Filter.by.chromosome <- function(dfx) lapply(chromosomes, function(y) filter(dfx,Chromosome == y ))
+  
+
+  Filter.dataframes.by.chromosome <- function(dfx, chr) lapply(dfx, (lapply(chr, Filter.by.chromosome)))            
+  
+  
+  
   
 #merged.genotypes.by.species <- merge.data.frame(snppositions, genotypes.by.species, by.x = "SNP_ID", by.y = "V1", all.y = TRUE, sort = FALSE)
 
 
 merged.genotypes.by.species <-lapply(genotypes.by.species, Merge.with.snppositions)
 names(merged.genotypes.by.species) <- c(paste("merged_", species.files.names, sep = ""))
-lapply(merged.genotypes.by.species, Firstrow.to.colnames)
+genotypes.by.species <- lapply(merged.genotypes.by.species, Firstrow.to.colnames)
 problem.postions <- lapply(merged.genotypes.by.species, Remove.problem.postions)
 names(problem.postions) <- c(paste(problem.postions, "miss_unk_mult_", sep = ""))
 
+split.genotype.files <- lapply(merged.genotypes.by.species, Filter.by.chromosome)
+split.genotype.files[[1]] -> trial
+trial[[1]]
+str(split.genotype.files)
+trial <- tibble()
+Filter.by.chromosome(observe) -> trial 
+trial[[1]] -> trial.1
+trial[[7]] -> trial.7
+trial[[11]] -> trial.11
+select(tail(trial.11), Position)
+
+split.genotype.files <- sapply(merged.genotypes.by.species, function(x) c(paste(x, "_", chromosomes, sep ="")))
 #assign column names
+for(i in rep(chromosomes))
+list(pastesplit.genotype.files <- lapply(merged.genotypes.by.species, function(x) filter(x, Chromosome == i)) -> c(paste(x,"_", i, sep ="")))
+done
 
-unique(snppositions$Chromosome)
+merged.genotypes.by.species[[1]] -> names(merged.genotypes.by.species[[1]])
 
 
+
+Filter.by.chromosome <- function(dfx) {
+  {
+    append(
+      lapply(chromosomes, function(y) (paste(dfx, "_", y, "accending", sep = "")) = filter(dfx, Chromosome == y )) %>% lapply(function(y) filter(y, !(Position %in% problem.locations)) %>% arrange(as.numeric(Position))),
+      lapply(chromosomes, function(y) (paste(dfx, "_", y, "descending", sep = "")) = filter(dfx, Chromosome == y )) %>% lapply(function(y) filter(y, !(Position %in% problem.locations)) %>% arrange(desc(as.numeric(Position))) %>% gsub(pattern = "?", replacement =  "-"))
+    )
+  }}
